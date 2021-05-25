b4c6e9e86a83ddcb329f24635e491c219684f2fe
hellzhammer <mattperley@hotmail.com>
2021-05-25 19:15:55 -0400

    uploading.

---
+++ Broker.cs
@@ -0,0 +1,121 @@
+﻿using MQTTnet;
+using MQTTnet.Protocol;
+using MQTTnet.Server;
+using System;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace KronusMqttLib
+{
+    public class Broker
+    {
+        public bool IsRunning { get; private set; }
+
+        private string BrokerID { get; set; }
+        private string IP_Address { get; set; }
+
+        private IMqttServer MqttServer { get; set; }
+        private MqttServerOptionsBuilder OptionsBuilder { get; set; }
+
+        /// <summary>
+        /// constructor takes and sets the main server ip to start up with
+        /// </summary>
+        /// <param name="serverIP">Server ip.</param>
+        public Broker(string serverIP, string brokerID)
+        {
+            this.BrokerID = brokerID;
+            this.IP_Address = serverIP;
+            this.IsRunning = false;
+            this.MqttServer = new MqttFactory().CreateMqttServer();
+        }
+
+        /// <summary>
+        /// builds the options to be used by the broker
+        /// </summary>
+        private void optionBuild()
+        {
+            // Configure MQTT server.
+            this.OptionsBuilder = new MqttServerOptionsBuilder()
+                .WithDefaultEndpointBoundIPAddress(IPAddress.Parse(this.IP_Address))
+                .WithConnectionBacklog(100)
+                .WithConnectionValidator(Connection_Validation)
+                .WithApplicationMessageInterceptor(Message_Intercept)
+                .WithSubscriptionInterceptor(Sub_Intercept)
+                .WithDisconnectedInterceptor((context) => {
+                    switch (context.DisconnectType)
+                    {
+                        case MqttClientDisconnectType.Clean:
+                                //do something
+                                break;
+                        case MqttClientDisconnectType.NotClean:
+                                //do something
+                                break;
+                        case MqttClientDisconnectType.Takeover:
+                                //do something
+                                break;
+                    }
+
+                }).WithClientId(this.BrokerID);
+        }
+
+        /// <summary>
+        /// validates connections
+        /// </summary>
+        /// <param name="context">C.</param>
+        private void Connection_Validation(MqttConnectionValidatorContext context)
+        {
+            context.ReasonCode = MqttConnectReasonCode.Success;
+        }
+
+        /// <summary>
+        /// intercepts messages coming from clients. 
+        /// </summary>
+        /// <param name="context">Context.</param>
+        private void Message_Intercept(MqttApplicationMessageInterceptorContext context)
+        {
+            //Console.WriteLine(Encoding.UTF8.GetString(context.ApplicationMessage.Payload));
+            context.AcceptPublish = true;
+        }
+
+        /// <summary>
+        /// intercepts subscriptions from clients
+        /// </summary>
+        /// <param name="context">Context.</param>
+        private void Sub_Intercept(MqttSubscriptionInterceptorContext context)
+        {
+            context.AcceptSubscription = true;
+        }
+
+        /// <summary>
+        /// start the server
+        /// </summary>
+        /// <returns>The start.</returns>
+        public async Task Start()
+        {
+            optionBuild();
+            await MqttServer.StartAsync(OptionsBuilder.Build());
+            if (MqttServer.IsStarted)
+            {
+                this.IsRunning = true;
+                Console.WriteLine("Server started");
+                Console.WriteLine(MqttServer.Options.DefaultEndpointOptions.BoundInterNetworkAddress);
+            }
+            else
+            {
+                Console.WriteLine("There has been an issue. Please exit and restart the broker.");
+            }
+        }
+
+        /// <summary>
+        /// kill the server
+        /// </summary>
+        /// <returns>The kill.</returns>
+        public async Task Kill()
+        {
+            await MqttServer.StopAsync();
+            MqttServer.Dispose();
+            this.IsRunning = false;
+        }
+    }
+}
+
---
+++ Client.cs
@@ -0,0 +1,170 @@
+﻿using MQTTnet;
+using MQTTnet.Client;
+using MQTTnet.Client.Options;
+using System;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace KronusMqttLib
+{
+    public class Client
+    {
+        public ObservableCollection<string> Messages { get; private set; }
+
+        public string ClientID { get; private set; }
+        public bool IsConnected { get; private set; }
+
+        private MqttFactory Factory { get; set; }
+        private IMqttClient MqttClient { get; set; }
+        private IMqttClientOptions Options { get; set; }
+
+        public Client(string clientID)
+        {
+            //set main parameters
+            this.IsConnected = false;
+            this.ClientID = clientID;
+
+            this.Messages = new ObservableCollection<string>();
+
+            // Create a new MQTT client.
+            this.Factory = new MqttFactory();
+            this.MqttClient = Factory.CreateMqttClient();
+            this.Options = new MqttClientOptionsBuilder()
+                .WithClientId(ClientID)
+                //.WithTcpServer("broker.hivemq.com")
+                .WithTcpServer("192.168.0.21")
+                //.WithWebSocketServer("broker.hivemq.com:8000/mqtt")
+                //.WithCredentials("bud", "%spencer%")
+                //.WithTls()
+                .WithCleanSession()
+                .Build();
+        }
+
+        public async Task Kill()
+        {
+            //do some stuff like un-sub from anything 
+            //app is currently listening to.
+
+            //tell server this client is offline.
+
+            //kill it with fire
+            await this.MqttClient.DisconnectAsync();
+            this.MqttClient.Dispose();
+        }
+
+        /// <summary>
+        /// Builds the client.
+        /// </summary>
+        /// <returns>The client.</returns>
+        /// <param name="mainTopic">Main topic.</param>
+        public virtual async Task BuildClient(string mainTopic)
+        {
+            // setup the handlers
+            MqttClient.UseDisconnectedHandler(async e => await DisconnectionHandler());
+            MqttClient.UseApplicationMessageReceivedHandler(e => MessageHandler(e));
+            MqttClient.UseConnectedHandler(async e => await this.Subscribe(mainTopic));
+
+            //when done, connect to server
+            await Connect();
+        }
+
+        /// <summary>
+        /// Connect this instance.
+        /// </summary>
+        /// <returns>The connect.</returns>
+        public virtual async Task Connect()
+        {
+            await MqttClient.ConnectAsync(Options, CancellationToken.None);
+            this.IsConnected = MqttClient.IsConnected;
+        }
+
+        /// <summary>
+        /// Sends the message.
+        /// </summary>
+        /// <returns>The message.</returns>
+        /// <param name="topic">Topic.</param>
+        /// <param name="message_content">Message content.</param>
+        public virtual async Task SendMessage(string topic, string message_content)
+        {
+            var message = new MqttApplicationMessageBuilder()
+                .WithTopic(topic)
+                .WithPayload(message_content)
+                .WithExactlyOnceQoS()
+                //.WithRetainFlag()
+                .Build();
+
+            var res = await MqttClient.PublishAsync(message, CancellationToken.None);
+        }
+
+        /// <summary>
+        /// Disconnections the handler.
+        /// </summary>
+        /// <returns>The handler.</returns>
+        public virtual async Task DisconnectionHandler()
+        {
+            //ensure this is set to false
+            this.IsConnected = false;
+            Console.WriteLine("### DISCONNECTED FROM SERVER ###");
+            await Task.Delay(TimeSpan.FromSeconds(5));
+
+            try
+            {
+                await MqttClient.ConnectAsync(Options, CancellationToken.None);
+            }
+            catch
+            {
+                //only for testing
+                Console.WriteLine("### RECONNECTING FAILED ###");
+            }
+        }
+
+        /// <summary>
+        /// Messages the handler.
+        /// </summary>
+        /// <param name="e">Event for when messages are recieved.</param>
+        public virtual void MessageHandler(MqttApplicationMessageReceivedEventArgs e)
+        {
+            var payload = Encoding.UTF8.GetString(e.ApplicationMessage.Payload);
+
+            // for testing only
+            Console.WriteLine("### RECEIVED APPLICATION MESSAGE ###");
+            Console.WriteLine($"+ Topic = {e.ApplicationMessage.Topic}");
+            Console.WriteLine($"+ Payload = { payload}");
+            Console.WriteLine($"+ QoS = {e.ApplicationMessage.QualityOfServiceLevel}");
+            Console.WriteLine($"+ Retain = {e.ApplicationMessage.Retain}");
+            Console.WriteLine();
+            //will remove above when done this class..
+            this.Messages.Add(payload);
+        }
+
+        /// <summary>
+        /// Subscribe the specified _topic.
+        /// </summary>
+        /// <returns>The subscribe.</returns>
+        /// <param name="_topic">Topic.</param>
+        public virtual async Task Subscribe(string _topic)
+        {
+            //ensure this is set to true
+            Console.WriteLine("### CONNECTED WITH SERVER ###");
+
+            // Subscribe to a topic
+            await MqttClient.SubscribeAsync(new MqttTopicFilterBuilder().WithTopic(_topic).Build());
+
+            Console.WriteLine("### SUBSCRIBED ###");
+        }
+
+        /// <summary>
+        /// removes a message from the current collections
+        /// </summary>
+        /// <param name="toRemove">To remove.</param>
+        public void Clear_Message(string toRemove)
+        {
+            if (this.Messages.Contains(toRemove))
+            {
+                this.Messages.Remove(toRemove);
+            }
+        }
+    }
+}
